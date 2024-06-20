966d24471d9c0a7d3e2a87b65ab86c4f6842f207
rtdongullo <dongromert@gmail.com>
2024-06-20 15:47:57 +0000

    sending email in node.js

---
+++ email.mjs
@@ -0,0 +1,30 @@
+import {program} from 'commander';
+import { mailTransport } from './mailTransport.mjs';
+
+
+program
+.option('-s, --subject')
+.option('-t, --to')
+.option('-m, --message');
+
+program.parse();
+
+const options = program.opts();
+const args = program.args;
+console.log(options);
+console.log(args);
+if (options.subject && options.to && options.message) {
+    // send email
+    mailTransport.sendMail({
+        from: 'noreply@shopa.life',
+        to: args[1],
+        subject:args[0],
+        text:args[2]
+    })
+    .then(console.log)
+    .catch(console.log);
+    // console.log('Message is Sent');
+} else {
+    console.log('some options are missings');
+}
+
